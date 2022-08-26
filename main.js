ab137c39e4d4a1602d908b60535aa21367f5a3fb
amanycoding <112172460+amanycoding@users.noreply.github.com>
2022-08-26 16:22:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img= document.querySelector(".hero");
+function phones(phone){
+    img.src=phone;
+}
+let body=document.querySelector("body");
+function colors(color){
+    body.style.backgroundColor=color;
+}
